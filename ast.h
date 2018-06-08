380b9398a5d60edad65262e72a3bbb7d89b1fd99
vkhristenko <vdkhristenko1991@gmail.com>
2018-06-08 11:26:46 +0200

    ast nodes

--- ast.h
+++ ast.h
@@ -1,6 +1,9 @@
 #ifndef ast_h
 #define ast_h
 
+#include <string>
+#include <vector>
+
 class ExprAST {
 public:
     virtual ~ExprAST() {}
@@ -29,6 +32,41 @@ public:
     BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS,
                   std::unique_ptr<ExprAST> RHS)
         : Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+class CallExprAST : public ExprAST {
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+public:
+    CallExprAST(std::string const& Callee,
+                std::vector<std::unique_ptr<ExprAST>> Args) 
+        : Callee(Callee), Args(std::move(Args)) 
+    {}
+};
+
+// this class represents the prototype for a function
+// which captures its name, and its argument anmes (thus implicitly tne number of 
+// arguments the function takes)
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    Prototype(std::string const& name, std::vector<std::string> Args)
+        : Name(name), Args(std::move(Args)) {}
+
+    std::string const& getName() const { return Name;}
+};
+
+// this class represents a function definition itself
+class FunctionAST {
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+                std::unique_ptr<ExprAST> Body) 
+        : Proto(std::move(Proto)), Body(std::move(Body)) {}
 };
 
 #endif // ast_h
