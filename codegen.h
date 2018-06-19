df02ffab56e1a909e1ee467869f1dfc0f3c99faf
vkhristenko <vdkhristenko1991@gmail.com>
2018-06-19 10:24:52 +0200

    trying to compile with all the optimizations added

--- codegen.h
+++ codegen.h
@@ -1,28 +1,40 @@
 #ifndef codegen_h
 #define codegen_h
 
-#include "Kaleidoscope/include/KaleidoscopeJIT.h"
+#include "include/KaleidoscopeJIT.h"
 #include "llvm/ADT/APFloat.h"
 #include "llvm/ADT/STLExtras.h"
+
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
 
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Scalar/GVN.h"
+
+
 #include "parser.h"
+
+using namespace llvm;
+using namespace llvm::orc;
 
 static llvm::LLVMContext TheContext;
 static llvm::IRBuilder<> Builder(TheContext);
 static std::unique_ptr<llvm::Module> TheModule;
 static std::map<std::string, llvm::Value*> NamedValues;
 static std::unique_ptr<llvm::FunctionPassManager> TheFPM;
-static std::unique_ptr<KaleidoscopeJIT> TheJIT;
-static std::map<std::string, std::unique_ptr<llvm::PrototypeAST>> FunctionProtos;
+static std::unique_ptr<llvm::orc::KaleidoscopeJIT> TheJIT;
+static std::map<std::string, std::unique_ptr<PrototypeAST>> FunctionProtos;
 
 llvm::Value *LogErrorV(char const* Str) {
     LogError(Str);
@@ -153,7 +165,7 @@ llvm::Function *FunctionAST::codegen() {
         llvm::verifyFunction(*TheFunction);
 
         // run the optimization passes
-        TheFPM->run(*TheFunctions);
+        TheFPM->run(*TheFunction);
 
         return TheFunction;
     }
@@ -175,7 +187,7 @@ void InitializeModuleAndPassManager(void) {
     TheFPM = std::make_unique<llvm::FunctionPassManager>(TheModule.get());
 
     // simple "peephole" optimizations and bit-twiddling opts
-    TheFPM->add(creatInstructionCombiningPass());
+    TheFPM->add(createInstructionCombiningPass());
 
     // reassociate exprs
     TheFPM->add(createReassociatePass());
